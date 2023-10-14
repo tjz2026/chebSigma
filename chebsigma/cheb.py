518bf9cd8b3feb9cdf6e1e59667668b8b06767a9
tjz <ructjz@example.com>
2023-10-14 21:42:49 +0200

    change the function name following pep8

--- chebsigma/cheb.py
+++ chebsigma/cheb.py
@@ -3,7 +3,7 @@
 from typing import List,Tuple,Dict,Optional,Callable
 import numpy as np
 
-def genChebyshevNodes(Ns: int, interval:List = [-1,1])->np.ndarray:
+def gen_chebyshev_nodes(Ns: int, interval:List = [-1,1])->np.ndarray:
     """
     create Ns +1 number of chebyshev nodes (chebyshev extreme points) within given interval,
     e.g., consider an interval of [-1,1], and let X_0, X_1, ..., X_{Ns} be the set of Ns points
@@ -17,12 +17,41 @@ def genChebyshevNodes(Ns: int, interval:List = [-1,1])->np.ndarray:
     a, b = interval
     return 0.5*(a + b) + 0.5*(b - a) *np.cos((np.pi/Ns)*np.arange(Ns +1))
 
+def sample_from_callable_function(f:Callable,chebNodes:np.ndarray)->np.ndarray:
+    """
+    sample function f on chebyshev interpolation nodes via simple for loop (not efficient)
+    :param f: a scalar callable function
+    :param chebNodes:
+    :return:
+    """
+    r = np.zeros_like(chebNodes.shape,dtype=np.float32)
+    for i in range(len(chebNodes)):
+        r[i] = f(chebNodes[i])
+    return r
+
+def sample_from_values(values:np.ndarray,interval:list,Ns:int)->np.ndarray:
+    """
+    Take N function values sampled on equal spaced nodes in given interval and use
+    linear interpolation to convert into chebyshev interpotants of Ns +1 nodes.
+    The nodes of the input values are defined in the interval [a,b] as
+    x_i = a + (i + 0.5)*dx, dx = (b-a)/N, i = 0, 1, ..., N-1
+    Note the equal spaced values should be fine enough for interpolate on chebyshev nodes.
+    :param values:
+    :param interval:
+    :return:
+    """
+    a, b = interval
+    n = len(values)
+    dx = (b-a)/n
+    x = (b-a)*np.linspace(0, 1, n, endpoint=False) + 0.5*dx + a
+    pass
+
 
 
 
 
 if __name__ == "__main__":
-    chebNodes = genChebyshevNodes(12,[0,1])
+    chebNodes = gen_chebyshev_nodes(12)
     import matplotlib.pyplot as plt
     plt.plot(chebNodes,"r+")
     plt.show()
